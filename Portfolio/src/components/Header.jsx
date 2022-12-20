b90270ae66270ce49cffe7a564aaa978e58ffdbe
Mefiasty <97467781+Mefiasty@users.noreply.github.com>
2022-12-20 18:33:27 +0100

    first commit

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -2,20 +2,28 @@ function Header() {
   return (
     <div className="">
       <h1 className="text-neutral-300 text-5xl pb-4">Portfolio </h1>
-      <div className="Links bg-neutral-700 pt-2 pb-2 rounded-3xl ">
-        <a href="" className="hover:bg-slate-900">
-          Home
-        </a>
-        <a href="" className="hover:bg-slate-900">
-          Projects
-        </a>
-        <a href="" className="hover:bg-slate-900">
-          Contact
-        </a>
-        <a href="" className="hover:bg-slate-900">
-          Download CV
-        </a>
+      <div className="Links bg-neutral-700 pt-2 pb-2 rounded-3xl grid md:grid-cols-4 gap-2 md:flex-wrap md:">
+        <div className="col-span-1 hover:bg-slate-900 hover:rounded-full sm:row-span-1">
+          <a href="" className="hover:bg-slate-900">
+            Home
+          </a>
+        </div>
+        <div className="col-span-1 hover:bg-slate-900 hover:rounded-full sm:row-span-1">
+          <a href="" className="hover:bg-slate-900">
+            Projects
+          </a>
+        </div>
+        <div className="col-span-1 hover:bg-slate-900 hover:rounded-full sm:row-span-1">
+          <a href="" className="">
+            Contact
+          </a>
+        </div>
+        <div className="col-span-1 hover:bg-slate-900 hover:rounded-full sm:row-span-1">
+          <a href="">Download CV</a>
+        </div>
       </div>
+      <br></br>
+      <br />
     </div>
   );
 }
