8fa3c8b0d9f2061b463989c716c8ae1557687fb1
Mefiasty <97467781+Mefiasty@users.noreply.github.com>
2022-12-20 17:03:04 +0100

    first commit

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <div className="">
+      <h1 className="text-neutral-300 text-5xl pb-4">Portfolio </h1>
+      <div className="Links bg-neutral-700 pt-2 pb-2 rounded-3xl ">
+        <a href="" className="hover:bg-slate-900">
+          Home
+        </a>
+        <a href="" className="hover:bg-slate-900">
+          Projects
+        </a>
+        <a href="" className="hover:bg-slate-900">
+          Contact
+        </a>
+        <a href="" className="hover:bg-slate-900">
+          Download CV
+        </a>
+      </div>
+    </div>
+  );
+}
+export default Header;
